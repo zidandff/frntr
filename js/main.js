6511fe42b01a1211ad74b591159e3c7c87b9943e
Zidan M. Daffa <zidandaff@gmail.com>
2020-08-17 12:43:49 +0700

    init

---
+++ js/main.js
@@ -0,0 +1,59 @@
+const mySwiper = new Swiper('.swiper-testi', {
+    // Optional parameters
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+      },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+
+  })
+
+const productSwiper = new Swiper('.swiper-product', {
+	  // Optional parameters
+    
+      // If we need pagination
+
+    
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    
+      breakpoints: {
+		320: {
+			slidesPerView: 'auto',
+			spaceBetween: 10
+		  },
+		  // when window width is >= 480px
+		  480: {
+			slidesPerView: 2,
+			spaceBetween: 20
+		  },
+		  // when window width is >= 640px
+		  640: {
+			slidesPerView: 3,
+			spaceBetween: 25
+		  },
+		  990: {
+			  slidesPerView: 4,
+			  spaceBetween: 30
+		  }
+		
+		  
+	  },
+})
