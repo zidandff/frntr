aea2c02c2bbc897cc69db828808661029e6fa2e8
Zidan M. Daffa <zidandaff@gmail.com>
2020-08-18 13:03:49 +0700

    add navigation trending product

--- js/main.js
+++ js/main.js
@@ -26,13 +26,13 @@ const productSwiper = new Swiper('.swiper-product', {
 	  // Optional parameters
     
       // If we need pagination
-
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+      },
     
       // Navigation arrows
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
     
       breakpoints: {
 		320: {
